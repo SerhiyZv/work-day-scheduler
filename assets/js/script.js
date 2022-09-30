ac827f6e9fc883931bf639e4ca47f03a848459b0
SerhiyZv <skiev31@gmail.com>
2022-09-30 19:23:26 -0400

    New update for HTML CSS and JS files

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var timeDisplayEl = $('#currentDay');
+
+// handle displaying the time
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
+
+  setInterval(displayTime, 1000);
