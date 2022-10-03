b2e9fb59328e47c3e8a81090b06fc668d145e8e7
SerhiyZv <skiev31@gmail.com>
2022-10-03 00:40:51 -0400

    Script file added

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,64 @@ function displayTime() {
     timeDisplayEl.text(rightNow);
   }
 
+var timeBlock = $(".time-block").addClass("row");
+var blockText = $("<p>").addClass("description");
+timeBlock.append(blockText);
+
+var currentTime = parseInt(moment().format("H"));
+
+var createEvents = function (timeSlots) {
+  timeSlots.forEach((element) => {
+    console.log(element);
+    let text = localStorage.getItem(parseInt(element.time));
+    console.log(text);
+    if (text) {
+      element.text.val(text);
+    }
+  });
+};
+
+var fetchEvents = function () {
+  var newArr = [];
+  //Array iterator method
+  $("textarea").each(function (index, elem) {
+    newArr.push({
+      time: $(elem).attr("id"),
+      text: $(elem),
+    });
+  });
+  createEvents(newArr);
+};
+
+//Color block to indicate past, present or future
+$("textarea").each(function () {
+  var $this = $(this);
+  var id = parseInt($this.attr("id"));
+
+  if (id < currentTime) {
+    $(this).addClass("past");
+  }
+  if (id > currentTime) {
+    $(this).addClass("future");
+  }
+  if (id === currentTime) {
+    $(this).addClass("present");
+  }
+});
+
+$("button.saveBtn").click(function (event, createEvents) {
+  event.preventDefault();
+
+  var $element = $(this).siblings("textarea");
+  var time = $element.attr("id");
+  // 
+  var text = $element.val().trim();
+  // 
+
+  if (time && text !== "") {
+    //
+    localStorage.setItem(time, text);
+  }
+});
+
   setInterval(displayTime, 1000);
